bb8456f11c2037302521546de7e4bbaa88ad5ad6
Matsuyanagi Genjuro <ef58ef64@gmail.com>
2012-03-01 00:03:00 +0900

    first commit

---
+++ relative/OptionSetting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace relative
+{
+	class OptionSetting
+	{
+//		public List<string > Wildcards { get; private set; }
+//		public ExcelFileProcessor.TextFormatType TextFormatType { get; set; }
+//		public bool FlagRecursive { get; set; }
+//		public bool FlagQuartOnlyExistingComma { get; set; }
+
+		public bool FlagHelp { get; set; }
+		public List<string > Paths { get; private set; }
+		public string TargetPath { get; set; }
+		public string BasePath { get; set; }
+		
+		public OptionSetting()
+		{
+			FlagHelp = false;
+			Paths = new List< string >();
+			
+//			TextFormatType = ExcelFileProcessor.TextFormatType.CSV;
+//			FlagRecursive = false;
+			
+
+
+		}
+	}
+}
---
+++ relative/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Mono.Options;
+
+namespace relative
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+
+			var optionSetting = new OptionSetting();
+
+			var p = new OptionSet{
+			                     	{"h|?|help", v => optionSetting.FlagHelp = v != null},
+/*
+			                     	{
+			                     		"format=",
+			                     		v =>
+			                     		optionSetting.TextFormatType =
+			                     		(ExcelFileProcessor.TextFormatType)
+			                     		Enum.Parse( typeof( ExcelFileProcessor.TextFormatType ), v )
+			                     		},
+			                     	{"r", v => optionSetting.FlagRecursive = v != null},
+									{"c", v => optionSetting.FlagQuartOnlyExistingComma = v != null},
+*/
+			                     };
+			optionSetting.Paths.AddRange( p.Parse( args ) );
+
+			// ヘルプ表示
+			if ( optionSetting.FlagHelp || args.Count() == 0 ){
+				DispHelp( p );
+				return;
+			}
+			
+			
+			if ( optionSetting.Paths.Count > 0 ){
+				//	相対パスを求めるターゲットファイル
+				optionSetting.TargetPath = optionSetting.Paths[ 0 ];
+			}
+			if ( optionSetting.Paths.Count > 1 ){
+				//	ベースパスが指定されていれば、ベースとなるディレクトリ名に修正する
+				
+				if ( System.IO.Directory.Exists( optionSetting.Paths[ 1 ] ) ){
+					//	既存のディレクトリであれば
+					//		path/directory
+					//		path/directory/
+					//	の両方を受け付ける
+					
+					if ( ! optionSetting.Paths[ 1 ].EndsWith( @"\" ) ){
+						//	最後に '\' が付いてなければつける
+						optionSetting.Paths[ 1 ] += @"\";
+					}
+					optionSetting.BasePath = optionSetting.Paths[ 1 ];
+					
+				}else{
+					//	ファイル名ならディレクトリ部分を取り出す。最後に '\' をつける
+					optionSetting.BasePath = System.IO.Path.GetDirectoryName( optionSetting.Paths[ 1 ] ) + @"\";
+				}
+				
+			}else{
+				//	オプションで指定されなければカレントディレクトリ
+				optionSetting.BasePath = System.IO.Directory.GetCurrentDirectory() + @"\";
+			}
+			
+			Uri basePath = new Uri( optionSetting.BasePath );
+			Uri targetPath = new Uri( optionSetting.TargetPath );
+			
+			//	相対パスを求める
+			//		ベースディレクトリには末尾に '\' が付いている必要がある
+			Uri relativePath = basePath.MakeRelativeUri( targetPath );
+			
+			
+			//	出力
+//			Console.WriteLine( "base     : " + basePath.ToString() );
+//			Console.WriteLine( "target   : " + targetPath.ToString() );
+//			Console.WriteLine( "relative : " + relativePath.ToString() );
+			Console.WriteLine( relativePath.ToString() );
+			
+			return ;
+		}
+		
+		//	ヘルプ表示
+		private static void DispHelp( OptionSet p )
+		{
+			Console.WriteLine( "usage: relative target [base]" );
+			p.WriteOptionDescriptions( Console.Out );
+		}
+		
+	}
+}
